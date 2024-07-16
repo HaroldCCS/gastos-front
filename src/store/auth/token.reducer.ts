e685d596380f72bd8091d7842729c10b080e01aa
Harold Camargo <hscamargo2002@gmail.com>
2024-07-15 21:14:54 -0500

    👷  add token storage

---
+++ src/store/auth/token.reducer.ts
@@ -0,0 +1,20 @@
+import { createReducer } from '@reduxjs/toolkit'
+
+import ACTIONS from './token.action'
+
+const name_storage = 'token'
+interface IReducer { [name_storage]: string }
+
+const initialState: IReducer = { [name_storage]: '' }
+
+const tokenReducer = createReducer<IReducer>(initialState, (builder) => {
+    builder.addCase(ACTIONS.set, (state, action) => {
+        state[name_storage] = action.payload 
+    })
+
+    builder.addCase(ACTIONS.drop, (state) => {
+        state[name_storage] = ''
+    })
+})
+
+export default tokenReducer
